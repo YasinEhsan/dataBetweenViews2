2dccc152d6cdc3316b26d97776054001a74e22a6
yasinehsan <yasinehsan11@gmail.com>
2020-04-23 18:26:54 -0400

    added bp

---
+++ dataBetweenViews2/SecViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecViewController.swift
+//  dataBetweenViews2
+//
+//  Created by Yasin Ehsan on 4/23/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class SecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
